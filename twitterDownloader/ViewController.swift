5d95b9b59b967054a69409dc156d5be9c45c0466
rasheedyakatak <rasheed@yakatak.com>
2017-02-14 13:19:02 +0000

    Initial Commit

---
+++ twitterDownloader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterDownloader
+//
+//  Created by Rasheed Wihaib on 14/02/2017.
+//  Copyright © 2017 Rasheed Wihaib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
